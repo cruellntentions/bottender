f71e8d66a2ed2761b0b14e2588bd48d6daaf7cc9
cruellntentions <44932536+cruellntentions@users.noreply.github.com>
2018-11-10 16:16:11 -0600

    Update index.js

--- examples/line-hello-world/index.js
+++ examples/line-hello-world/index.js
@@ -2,8 +2,10 @@ const { LineBot } = require('bottender');
 const { createServer } = require('bottender/express');
 
 const bot = new LineBot({
-  channelSecret: '__FILL_YOUR_SECRET_HERE__',
-  accessToken: '__FILL_YOUR_TOKEN_HERE__',
+  channelSecret: '51b00aae9d2092bbd8919e6ed0a5eb13
+',
+  accessToken: 'mAUVpHo5RJppPWehVF3MejFvRxX3CArVmoXylauDuemqRdW4L1HHuPbyu0u0WEhmscStQXGiNsKQo+Z66cUe6EC4rPH9ADrqBPatEgeysqYlgtCX5dEEYNUresrHcr+RSYS8bBCh+uQ6DUtY2UVjnQdB04t89/1O/w1cDnyilFU=
+',
 });
 
 bot.onEvent(async context => {
